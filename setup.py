439279e544dec3faa919f9b0392cbdc569e9acba
HuntorGames <rt001a9138@gmail.com>
2025-03-21 11:50:26 +0000

    covnerted to a package install of the project to fix some import issues in edit mode 'pip install -e .' command

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='modplod',
+    version='0.1',
+    description='AI Cop for Twitch Moderation & Interactivity based on a roleplay character for https://twitch.tv/beerhuntor',
+    url='https://github.com/ModPlod/modplod',
+    author='HuntorWorks',        
+    author_email='gM5l9@example.com',
+    license='MIT',
+    packages=find_packages(),
+)
