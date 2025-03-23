3c898d9bb1080972d217caaeeecc57363d4a80e6
HuntorWorks <rt001a9138@gmail.com>
2025-03-23 16:06:26 +0000

    changed __main__ thread to allow for both 'python web_app/app.py' cli execution and 'flask run --cert=adhoc' due to 'flask run' not running __main__ internally

--- web_app/app.py
+++ web_app/app.py
@@ -1,19 +1,23 @@
 from flask import Flask
 from web_app.obs_window import start_obs_audio_window
 from web_app.routes import api_blueprint
-import sys
+from core.twitch_api_manager import TwitchAPIManager
 import threading
+
+print("App started")
+
 
 app = Flask(__name__)
 app.register_blueprint(api_blueprint, url_prefix='/')
 
 def start_audio_window():
-    threading.Thread(target=start_obs_audio_window, daemon=True).start()
-
-if __name__ == "__main__":
+    thread=threading.Thread(target=start_obs_audio_window, daemon=True)
+    thread.start()
+    
+if __name__ != "__main__":
     start_audio_window()
     print(app.url_map)
-    app.run(host="0.0.0.0", port=5000)
-
-
+    app.run(host="0.0.0.0", port=5000, use_reloader=False)
+else: 
+    pass
 
