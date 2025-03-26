a057fd8d2f6444b4b7ae2ba6bfe6618a52ba3e91
HuntorWorks <rt001a9138@gmail.com>
2025-03-26 16:29:10 +0000

    re wrote twitch auth, now got oauth working, using twitchAPI built in auth

--- web_app/app.py
+++ web_app/app.py
@@ -4,19 +4,18 @@ from web_app.routes import api_blueprint
 from core.twitch_api_manager import TwitchAPIManager
 import threading
 
-print("App started")
-
-
 app = Flask(__name__)
 app.register_blueprint(api_blueprint, url_prefix='/')
+twitch_api_manager = TwitchAPIManager()
+
 
 def start_audio_window():
     thread=threading.Thread(target=start_obs_audio_window, daemon=True)
     thread.start()
-    
+
 if __name__ != "__main__":
     start_audio_window()
-    print(app.url_map)
+    twitch_api_manager.start_twitch_api_manager()
     app.run(host="0.0.0.0", port=5000, use_reloader=False)
 else: 
     pass
--- web_app/routes.py
+++ web_app/routes.py
@@ -1,5 +1,3 @@
-import os
-import sys
 from flask import request, jsonify, Blueprint
 from dotenv import load_dotenv
 from bots.gpt_character import Character
