2811832266784ae02b087a3a8f4e0dbf20b64686
HuntorWorks <rt001a9138@gmail.com>
2025-03-26 16:31:24 +0000

    re wrote twitch auth, now got oauth working, using twitchAPI built in auth

--- core/twitch_api_manager.py
+++ core/twitch_api_manager.py
@@ -1,102 +1,90 @@
-import os
-import json
-import time
 from twitchAPI.twitch import Twitch
-from twitchAPI.oauth import UserAuthenticator
-from twitchAPI.type import AuthScope, ChatEvent
-from twitchAPI.chat import EventData, ChatMessage, Chat
-import asyncio
-import threading 
+from twitchAPI.oauth import UserAuthenticator, refresh_access_token
+from twitchAPI.type import AuthScope
 from dotenv import load_dotenv
+import time, os, json, asyncio
 
-
+#
 class TwitchAPIManager:
+
     def __init__(self):
         load_dotenv()
-        self.APP_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
-        self.APP_CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
-        self.USER_SCOPE = [
-            AuthScope.CHAT_READ,
-            AuthScope.CHANNEL_MODERATE,
+        self.client_id = os.getenv("TWITCH_CLIENT_ID")
+        self.client_secret = os.getenv("TWITCH_CLIENT_SECRET")
+        self.scopes = [
+            AuthScope.CHAT_READ, 
+            AuthScope.CHAT_EDIT, 
             AuthScope.MODERATOR_MANAGE_BANNED_USERS,
+            AuthScope.MODERATOR_MANAGE_CHAT_MESSAGES,
+            AuthScope.MODERATOR_READ_MODERATORS, 
+            AuthScope.CLIPS_EDIT,
+            AuthScope.MODERATOR_READ_FOLLOWERS, 
             AuthScope.MODERATOR_MANAGE_BLOCKED_TERMS,
-        ]
-        print(f"running api manager")
-
+            AuthScope.MODERATOR_MANAGE_ANNOUNCEMENTS,
+            AuthScope.MODERATOR_MANAGE_AUTOMOD,
+            AuthScope.MODERATOR_MANAGE_SHOUTOUTS,
+            AuthScope.CHANNEL_MANAGE_VIPS,
+            AuthScope.WHISPERS_READ,
+            AuthScope.WHISPERS_EDIT 
+            ]
+        self.token_data = None
+        self.TOKEN = None
+        self.REFRESH_TOKEN = None
     def start_twitch_api_manager(self):
-        thread = threading.Thread(target=self.run).start()
-        thread.start()
+        asyncio.run(self.twitch_api_manager())
 
-    async def on_ready(self, ready_event: EventData):
-        print("Ready to join channels")
+    async def twitch_api_manager(self):
+        twitch = await Twitch(self.client_id, self.client_secret)
+        authenticator = UserAuthenticator(twitch, self.scopes, force_verify=False, url='http://localhost:17563', host='0.0.0.0', port=17563)
 
-        await ready_event.chat.join_room(os.getenv("TWITCH_TARGET_CHANNEL"))
+        if os.path.exists("token_data.json"):
+            self.load_token_data()
+            self.TOKEN = self.token_data["token"]
+            self.REFRESH_TOKEN = self.token_data["refresh_token"]
+            self.TOKEN_EXPIRES_AT = self.token_data["expires_at"]
 
-    async def on_message(self, msg: ChatMessage):
-        print(f"{msg.room.name}, {msg.user.name}, said: {msg.text}")
+            if time.time() > self.TOKEN_EXPIRES_AT:
+                await self.refresh_token()
+        else:
+            # Get the token
+            await self.full_authentication()
 
-    def save_token(self, token_data):
-        try: 
-            with open("token.json", "w") as file:
-                json.dump(token_data, file)
-        except Exception as e:
-            print(f"Failed to save token: {e}")
+        await twitch.set_user_authentication(self.TOKEN, self.scopes, self.REFRESH_TOKEN)
 
-    def load_token(self):
-        try: 
-            with open("token.json", "r") as file:
-                return json.load(file)
-        except Exception as e:
-            print(f"Failed to load token: {e}")
-            return None
-
-    async def run(self):
-        twitch = await Twitch(self.APP_CLIENT_ID, self.APP_CLIENT_SECRET)
-        auth = UserAuthenticator(twitch, self.USER_SCOPE)
-
-        token = None
-        refresh_token = None
-
-        token_data = self.load_token()
-
-
-        if token_data: 
-            token = token_data["access_token"]
-            refresh_token = token_data["refresh_token"]
-
-            if token_data["expires_at"] < time.time():
-                print(f"Token expired, refreshing token..")
-                try: 
-                    new_token_data = await twitch.get_refreshed_user_auth_token(refresh_token)
-                    token = new_token_data["access_token"]
-                    refresh_token = new_token_data["refresh_token"]
-                    self.save_token(new_token_data)
-                except Exception as e:
-                    print(f"Failed to refresh token: {e}")
-                    token, refresh_token = None, None
-            else: 
-                print(f"Using saved token")
-
-        if not token or not refresh_token:
-            token, refresh_token = await auth.authenticate()
-            self.save_token({"access_token": token, "refresh_token": refresh_token, "expires_at": time.time() + 3600})
-
-
-        await twitch.set_user_authentication(token, self.USER_SCOPE, refresh_token)
-
-        # Creates Chat Instance
-        chat = await Chat(twitch)
-
-        # Listen to when the bot is done starting up and ready to join channels
-        chat.register_event(ChatEvent.READY, self.on_ready)
-        # Listen to chat messages
-        chat.register_event(ChatEvent.MESSAGE, self.on_message)
-
-        chat.start()
-
+    async def refresh_token(self):
         try:
-            input("press ENTER to stop\n")
-        finally:
-            chat.stop()
-            await twitch.close()
+            new_token_data = await refresh_access_token(self.REFRESH_TOKEN, self.client_id, self.client_secret)
+            self.TOKEN = new_token_data["token"]
+            self.REFRESH_TOKEN = new_token_data["refresh_token"]
+            self.save_token_data()
+        except Exception as e:
+            print(f"Error refreshing token: {e}")
+            await self.full_authentication()
+        return True
+    
+    async def full_authentication(self):
+        twitch = await Twitch(self.client_id, self.client_secret)
+        authenticator = UserAuthenticator(twitch, self.scopes, force_verify=False, url='http://localhost:17563', host='0.0.0.0', port=17563)
+        token, refresh_token = await authenticator.authenticate()
+        self.TOKEN = token
+        self.REFRESH_TOKEN = refresh_token
+        self.save_token_data()
 
+    def save_token_data(self):
+        expires_at = time.time() + 3600 # 1 hour standard for oauth tokens
+        with open("token_data.json", "w") as f:
+            json.dump({"token": self.TOKEN, "refresh_token": self.REFRESH_TOKEN, "expires_at": expires_at}, f)
+            self.TOKEN_EXPIRES_AT = expires_at
+
+    def load_token_data(self):
+        with open("token_data.json", "r") as f:
+            data = json.load(f)
+            self.token_data = data
+    
+    def get_token(self):
+        return self.TOKEN
+
+    def get_refresh_token(self):
+        return self.REFRESH_TOKEN
+    
+    
